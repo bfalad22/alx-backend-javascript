74df1b523e1c4204bd35536df650dee043176dcc
bfalad22 <bolasaliu93@gmail.com>
2023-11-20 07:14:55 -0800

    Node_JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  try {
+    const fileContents = fs.readFileSync(fileName, 'utf-8');
+    const lines = fileContents.toString().split('\n');
+    for (let i = 0; i < lines.length; i += 1) {
+      if (lines[i]) {
+        length += 1;
+        const field = lines[i].toString().split(',');
+        if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+          students[field[3]].push(field[0]);
+        } else {
+          students[field[3]] = [field[0]];
+        }
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
+        } else {
+          fields[field[3]] = 1;
+        }
+      }
+    }
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,1 @@
+This is the readme for 0x05-Node_JS_basic
